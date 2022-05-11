19f945dc8580df18bb785804ed31db618c39b557
Ana Isabel Iturbe <anaisabeliturbec@gmail.com>
2022-05-11 13:54:27 -0400

    Created components WelcomeSection, Informatio, RoadMap and imported them into home index

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <div className="bg-white bg-opacity-20 flex flex-row md:h-24 h-20 w-full items-center">
+    <div className="bg-white bg-opacity-25 flex flex-row md:h-24 h-20 w-full items-center">
       <img src="blue-logo.svg" className="md:ml-16 ml-5 md:w-48 w-36" alt="logo" />
       <div className="w-full flex justify-end items-center">
         <button 
-        className="bg-blue-700 font-light md:w-32 w-24 md:h-12 h-10 rounded-lg md:mr-16 mr-5 text-white font-display-400 text-lg hover:text-blue-700 hover:bg-white"
+        className="bg-blue-700 font-light md:w-32 w-24 md:h-12 h-10 rounded-lg md:mr-16 mr-5 text-white font-display-400 text-lg hover:text-blue-700 hover:bg-white drop-shadow-3xl"
         >
-          Login
+          Connect
         </button>
       </div>
     </div>
