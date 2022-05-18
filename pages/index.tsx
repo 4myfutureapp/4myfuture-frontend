97fd4154538755cfb86fc6b6c68e5e4689338e65
Ana Isabel Iturbe <anaisabeliturbec@gmail.com>
2022-05-17 20:25:40 -0400

    Deleted unused component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,13 @@ import RoadMap from '../components/home/RoadMap';
 import ContributorSection from '../components/home/ContributorSection';
 import TeamSection from '../components/home/TeamSection';
 import FrequentQuestionSection from '../components/home/FrequentQuestionSection';
-import MobileRoadMap from '../components/home/mobileRoadmap';
 
 export default function index() {
   return (
     <Layout>
       <WelcomeSection />
       <Information />
-      {/* <div className="hidden lg:block"> */}
-        <RoadMap />
-      {/* </div> */}
-      {/* <div className="block md:hidden">
-        <MobileRoadMap />
-      </div> */}
+      <RoadMap />
       <ContributorSection />
       <TeamSection />
       <FrequentQuestionSection />
