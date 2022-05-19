9d7f2ad92a59f1a402a88531546c46ed58ce2a07
Ana Isabel Iturbe <anaisabeliturbec@gmail.com>
2022-05-18 20:36:01 -0400

    Finished adding Footer Information & Created Proposal Page

---
+++ pages/proposals/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+function ProposalPage() {
+  return (
+    <Layout>
+      <p>proposals page</p>
+    </Layout>
+  );
+}
+
+export default ProposalPage;
